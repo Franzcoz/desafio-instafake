2b81e166af835dfed256d25eed49f0eaee3e079f
Franco Pérez Valeria <franco.pv@protonmail.com>
2022-01-11 22:12:58 -0300

    Funcionalidad de formulario de ingreso casi lista, con captura de evento submit, funcion para obtener token, y funcion init para acceder a token almacenado. Falta funcion que esconda formulario y muestre el contenido de la pagina.

--- public/covid19/assets/scripts.js
+++ public/covid19/assets/scripts.js
@@ -1,5 +1,34 @@
 // Formulario de ingreso Franco
+let formu = $('#formulario-login')
 
+formu.on('submit', function (ev) {
+    ev.preventDefault();
+    const email = $('#email').val();
+    const password = $('#password').val();
+    getToken(email, password);
+})
+
+let getToken = async (email, password) => {
+    await fetch('/api/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password })
+    })
+    const jwt = await data.json()
+    // guardamos el token en una variable
+    const token = jwt.token;
+    localStorage.setItem('token', token);
+    // getData(token);
+}
+
+//Chequear si existe token
+const init = () => {
+    const token = localStorage.getItem('token');
+    if (token == null) {
+        return;
+    }
+    // getData(token);
+}
+init();
 
 // Traer los datos de la api Javier
 

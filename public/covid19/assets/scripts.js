3736a79fddeacff2a57e7cdc661b6c12e13808b2
javiereduar <94938493+javiereduar@users.noreply.github.com>
2022-01-11 23:02:36 -0300

    consulta a api y filtrodo de objetos v1.0

--- public/covid19/assets/scripts.js
+++ public/covid19/assets/scripts.js
@@ -2,7 +2,33 @@
 
 
 // Traer los datos de la api Javier
+//***************************///***************************/
+/*casos activos, casos confirmados, casos muertos, recuperdaos*/ 
 
+async function getData(){
+    //consulta a la api
+    const data = await fetch('/api/total')
+    
+    //respuesta de api a json
+    const request = await data.json()
+    //acceder a data dentro del json, donde estan los datos de cada pais
+    const casosPais = request.data
+
+    //filtar Paises segun condicion
+    const filtro = casosPais.filter(pais => pais.deaths >= 10000)
+    
+    console.log(filtro)
+}
+
+
+
+
+
+
+
+
+
+//***************************///***************************/
 
 // Gráfico Carla
 
